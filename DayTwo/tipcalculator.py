ea51f090013c569a0d9589ca569c36afb01a54dc
Richard M <36093669+itsrichardmai@users.noreply.github.com>
2025-08-20 12:54:01 -0700

    Day 2 Project
    This reverts commit 734061ff9ea5aef581b7594a225a155ff48f9ae0.

--- DayTwo/tipcalculator.py
+++
@@ -1,9 +0,0 @@
-print("Hello! I will calculate your")
-subtotal = float(input("What is the total bill? $"))
-tip = float(input("What % would you like to tip?"))
-total = (subtotal + (subtotal)*tip/100)
-people = int(input("How many ways are we splitting the bill?"))
-
-print(f"Your total bill is ${total}")
-print(f"Your tip is: {subtotal*tip/100}")
-print(f"Your total is ${total/people} per person!")
