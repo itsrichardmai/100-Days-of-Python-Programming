734061ff9ea5aef581b7594a225a155ff48f9ae0
Richard M <36093669+itsrichardmai@users.noreply.github.com>
2025-08-20 12:52:34 -0700

    CS50P Libraries
    libraries, modules, sys, import, from, randint, shuffle, statistics,
    CLI arguments, sys
    sys.exit
    slices
    packages, pypi, pip
    cowsay
    APIs, requests, JSON
    Custom Libraries

---
+++ DayTwo/task.py
@@ -0,0 +1,29 @@
+# print(len("1234567890"))
+
+# print(float(4343434))
+# print((int("232323")))
+# print(str(2039))
+# print(bool("False"))
+
+# print("My age is: " + str(12))
+# print(123 + 456)
+# print(7-3)
+# print(3*2)
+# print(5/3)
+# print(11111//3) 
+
+# print(5/5)
+# # equals 8 or 2^3 
+# print(2**3) 
+
+# bmi calculator units: kg/m 
+
+weight = float(input("What is your weight? (in kg)"))
+height = float(input("What is your height (in m)?"))
+bmi = round((weight / height**2))
+print(f"Your bmi is {bmi}")
+
+
+
+
+
