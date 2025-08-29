8a68b514eb16efd99f004943241f82e88de72278
Richard M <richardmaichs276@gmail.com>
2025-08-29 13:46:05 -0700

    update calculator.py
    vscode finally not crashing my computer.
    added recursion to call calculator based on user decision.
    y for accumulate
    n for restart
    q for close program.

--- Day10/calculator.py
+++ Day10/calculator.py
@@ -1,5 +1,26 @@
-
-
+logo = '''
+           _            _       _             
+          | |          | |     | |            
+  ___ __ _| | ___ _   _| | __ _| |_ ___  _ __ 
+ / __/ _` | |/ __| | | | |/ _` | __/ _ \| '__|
+| (_| (_| | | (__| |_| | | (_| | || (_) | |   
+ \___\__,_|_|\___|\__,_|_|\__,_|\__\___/|_|   
+      
+_____________________
+|  _________________  |
+| | RM           0. | |
+| |_________________| |
+|  ___ ___ ___   ___  |
+| | 7 | 8 | 9 | | + | |
+| |___|___|___| |___| |
+| | 4 | 5 | 6 | | - | |
+| |___|___|___| |___| |
+| | 1 | 2 | 3 | | x | |
+| |___|___|___| |___| |
+| | . | 0 | = | | / | |
+| |___|___|___| |___| |
+|_____________________|
+      '''
 print('''
            _            _       _             
           | |          | |     | |            
@@ -39,36 +60,31 @@ operations = {
     '*':multiply,
     '/':divide
 }
-number1 = float(input("What's the first number?: "))
 
-keep_going = True
-while keep_going:
-    for symbol in operations:
-        print(symbol)
-    
-    operation = input("What operation: ?")
-    number2 = float(input("What's the second number?: "))
-    answer = (operations[operation](number1, number2))
-    print(f"your answer is {answer}")
-    should_continue = input(f"Operate on {answer}? type 'y' for yes or 'n' for no ")
-    if should_continue == 'n':
-        print("Thank you! Goodbye!")
-        keep_going = False
-    elif should_continue == 'y':
-        number1 = answer
-    else: 
-        print("You did not enter a valid option.")
+def calculator():
+    print(logo)
+    number1 = float(input("What's the first number?: "))
 
-# if operation == '+':
-#     add(number1, number2)
-# elif operation == '-':
-#     subtract(number1, number2)
-# elif operation == '*': 
-#     multiply(number1, number2)
-# elif operation == '/': 
-#     divide(number1, number2)
-# else: 
-#     print("You did not provide a valid operation!")
+    keep_going = True
+    while keep_going:
+        for symbol in operations:
+            print(symbol)
+        
+        operation = input("What operation: ?")
+        number2 = float(input("What's the second number?: "))
+        answer = (operations[operation](number1, number2))
+        print(f"your answer is {answer}")
+        should_continue = input(f"Operate on {answer}? type 'y' for yes. 'n' for no. 'q' to close. ")
+        if should_continue == 'n':
+            print("\n" *10)
+            calculator()
+        elif should_continue == 'y':
+            number1 = answer
+        elif should_continue == 'q':
+            print("Thank you! Goodbye!")
+            keep_going = False
+            break
+        else: 
+            print("You did not enter a valid option.")
 
-
-
+calculator()
